2e5beb229e287d1bac406f72e05a55abb8f5e01b
Patrick Weinstein <patrick.weinstein@patworx.de>
2024-08-26 05:34:02 +0200

    v1.0.0

--- GXModules/UnzerPayment/UnzerPayment/Classes/UnzerConstants.php
+++ GXModules/UnzerPayment/UnzerPayment/Classes/UnzerConstants.php
@@ -1,6 +1,6 @@
 <?php
 
-class UnzerConstants{
+class UnzerConstants {
     const MODULE_NAME = 'unzer';
     const MODULE_PATH_FS = DIR_FS_CATALOG.'GXModules/UnzerPayment/UnzerPayment/';
     const MODULE_PATH_WS = DIR_WS_CATALOG.'GXModules/UnzerPayment/UnzerPayment/';
@@ -9,7 +9,7 @@ class UnzerConstants{
     const ORDER_TABLE_PAYMENT_METHOD_COLUMN = 'unzer_payment_method';
     const ORDER_TABLE_PAYMENT_METHOD_LABEL_COLUMN = 'unzer_payment_method_label';
     const DISABLED_PAYMENT_METHODS = ['giropay', 'PIS', 'bancontact'];
-    const MODULE_VERSION = '0.0.1';
+    const MODULE_VERSION = '1.0.0';
 
     const LOG_LEVEL_DEBUG = 'debug';
     const LOG_LEVEL_ERROR = 'error';
